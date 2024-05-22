d69bdc94ce4be93b098ff23367f376b9aa4fb4f0
Taloohi <abdulazt@uwindsor.ca>
2024-05-21 18:23:37 -0700

    added recusrive flag for ls command to search all subdirectories. also added -a flag to include all files

---
+++ finder-app/finder.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Date: 5/15/2024
+# Author: Talha Abdulaziz
+
+echo "filesdir entered: $1"
+echo "searchstr enered: $2"
+
+filesdir=$1
+searchstr=$2
+
+if [ -z $filesdir ] || [ -z $searchstr ]
+then
+    echo "Missing argument. Please enter the following arguments"
+    echo "- File directory"
+    echo "- Search string"
+    exit 1
+else
+    echo "Starting search for $searchstr in directory $filesdir"
+fi
+
+if [ -d $filesdir ]
+then
+    echo ...
+else
+    echo "$filesdir is not recognized as a directory in the filesystem"
+    exit 1
+fi
+
+matching_files=$(ls $filesdir -a -R -1 | wc -l)
+matching_lines=$(grep -r $searchstr * $1 | wc -l)
+
+echo "The number of files are $matching_files and the number of matching lines are $matching_lines"
+
+# echo $file_count
+# echo $line_count
